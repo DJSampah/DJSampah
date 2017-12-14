a202a204e3f7f59f715a5337a9ba6d95becc8a2d
muhammadrifqi32 <rifqi15ti@mahasiswa.pcr.ac.id>
2017-12-14 17:21:09 +0700

    loginmember.aspx (tanpa template) login.aspx (dengan template)

---
+++ login.aspx.vb
@@ -0,0 +1,14 @@
+﻿
+Partial Class login
+    Inherits System.Web.UI.Page
+
+    Protected Sub btnlogin_Click(sender As Object, e As EventArgs) Handles btnlogin.Click
+        Dim username As String = "member"
+        Dim password As String = "member"
+        If (txtusername.Text = username) And (txtusername.Text = password) Then
+            Response.Redirect("~/Member/Akun.aspx", True)
+        Else
+            Response.Redirect("~/Member/Shop.aspx", True)
+        End If
+    End Sub
+End Class
