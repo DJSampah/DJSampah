50414e611238545a8d0692c99504715a9dd40cc5
muhammadrifqi32 <rifqi15ti@mahasiswa.pcr.ac.id>
2018-01-26 21:38:55 +0700

    tes bisa apa en

--- web baru/user/kontak.aspx.vb
+++
@@ -1,5 +0,0 @@
-﻿
-Partial Class web_baru_user_kontak
-    Inherits System.Web.UI.Page
-
-End Class
--- web baru/user/tentang.aspx.vb
+++
@@ -1,5 +0,0 @@
-﻿
-Partial Class web_baru_user_tentang
-    Inherits System.Web.UI.Page
-
-End Class
